601ac81a15045ea318bc76e0f0459a9411addfc2
Mark Kerner <mark.kerner@hotmail.com>
2016-09-22 14:33:34 +0200

    Refactor, changed text to SIMPLE_TEXT

--- TTT/player.cpp
+++ TTT/player.cpp
@@ -1,23 +1,27 @@
 #include "player.hpp"
 #include <cstdlib>
+#include <iostream>
+
+using namespace std;
 
 namespace TICTACTOE
 {
 
 GameState Player::play(const GameState &pState,const Deadline &pDue)
 {
-    std::vector<GameState> lNextStates;
-    pState.findPossibleMoves(lNextStates);
+    std::vector<GameState> l_next_states;
+    pState.findPossibleMoves(l_next_states);
 
-    if (lNextStates.size() == 0) return GameState(pState, Move());
+    if (l_next_states.size() == 0) 
+		return GameState(pState, Move());
 
     /*
      * Here you should write your clever algorithms to get the best next move, ie the best
      * next state. This skeleton returns a random move instead.
      */
-	int i;
+	cerr << "2" << endl;
 
-    return lNextStates[rand() % lNextStates.size()];
+    return l_next_states[rand() % l_next_states.size()];
 }
 
 /*namespace TICTACTOE*/ }
