48d18df1d265fd51522a99e14d2805817d3c3d77
Amund <amund@kth.se>
2016-09-26 22:52:17 +0200

    update heuristic

--- TTT3D/handwritten_heuristic.py
+++ TTT3D/handwritten_heuristic.py
@@ -1,18 +1,55 @@
-
-
-
-
-
+'''
 # new heuristics function
 
 # we know how to reach rows and columns
 
 # then verticals are % 16 = 0
 
-# diagonal1 - across, topleft_bottomright
-# 3,6,9,12
-# 19,22,25,28
-# 35,38,41,44
-# 51,54,57,60
+From the number they start, add +k for each step
 
-# diagonal2 - down
+# diagonal - across, TR_BL
+# 3, 
+# 19, 
+# 35, 
+# 51, 
+	+3
+
+# diagonal - across, TL_BR
+# 0, 
+# 16,
+# 32,
+# 48,
+	+5
+
+diagonal - inside
+0,21,42,63
+3,22,41,60
+12,25,38,51
+15,26,37,48
+
+vertical, TL_
+0,
+16,
+32,
+48,
+
+3,
+19,
+35
+51
+	+20
+
+vertical, TR_
+12,
+28,
+44,
+60,
+
+15
+31
+47
+63
+	+12
+
+
+
