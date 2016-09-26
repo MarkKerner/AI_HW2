b31f031547fff5e0f6854c1fa6ca8f04d21ce05b
Amund <amund@kth.se>
2016-09-26 18:41:14 +0200

    updated run and start of new heuristic

--- TTT3D/run.sh
+++ TTT3D/run.sh
@@ -3,7 +3,7 @@
 # javac HelloWorld.java; 
 # java Helloworld;
 
-if g++ *.cpp -Wall -std=c++11 -o TTT.exe;
+if g++ *.cpp -Wall -std=c++11 -stdlib=libc++ -o TTT.exe;
 then
    echo "Compilation successful!"
     ./TTT.exe init verbose < pipe | ./TTT.exe > pipe
